70e613747c841edd5c0843a136f8d6e3a1c88b12
JB Tellez <jb.tellez@gmail.com>
2018-07-31 11:36:46 -0700

    Changes app name

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,35 @@
 import React, { Component } from "react";
+import { say } from "cowsay";
+import faker from "faker";
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      content: ""
+    };
+    this.sayHandler = this.sayHandler.bind(this);
+  }
+  sayHandler() {
+    const content = say({ text: faker.name.findName() });
+
+    this.setState({ content });
+  }
+
   render() {
-    return <h1>hola</h1>;
+    const critter =
+      this.state.content === "" ? (
+        <p>click the button</p>
+      ) : (
+        <pre>{this.state.content}</pre>
+      );
+
+    return (
+      <div>
+        <h1>Generate Cowsay Lorem</h1>
+        {critter}
+        <button onClick={this.sayHandler}>click me</button>
+      </div>
+    );
   }
 }
